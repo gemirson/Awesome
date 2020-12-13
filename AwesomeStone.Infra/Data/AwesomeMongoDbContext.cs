d0cc97d2e7c76a1f0c29ad40609c8922608a7f7a
Gemirson <gemirson.silva@gmail.com>
2020-12-13 14:23:58 -0300

    feature:Ensure MongoDbContext

---
+++ AwesomeStone.Infra/Data/AwesomeMongoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeStone.Core.Entidades;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeStone.Infra.Data
+{
+    public class AwesomeMongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public BankMongoDbContext(IOptions<MongoDbConfig> config)
+        {
+            var client = new MongoClient(config.Value.ConnectionString);
+            _database = client.GetDatabase(config.Value.Database);
+        }
+
+        public IMongoCollection<Employee> Operacao => _database.GetCollection<Employee>("Employee");
+    }
+}
