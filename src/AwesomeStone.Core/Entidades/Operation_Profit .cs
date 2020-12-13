fcababc3d6e801fa17681c4afa3fbdf9b88d5c7a
Gemirson <gemirson.silva@gmail.com>
2020-12-13 15:17:53 -0300

    feature:Ensure Add Entidade Operation Profit

---
+++ src/AwesomeStone.Core/Entidades/Operation_Profit .cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeStone.Core.Entidades
+{
+    public class Operation_Profit : Entidade
+    {
+
+        public decimal Valor_Distribuido { get; private set;}
+        public Operation_Profit(decimal valor_Distribuido)
+        {
+            Valor_Distribuido = valor_Distribuido;
+        }
+
+        public override void Validate()
+        {
+            AddNotifications(new Contract()
+              .Requires()
+              .IsGreaterThan(Valor_Distribuido, 0, nameof(Valor_Distribuido), "O valor a ser distribuido bruto tem que ser maior que zero"));
+
+            AddNotifications(new Contract()
+                  .Requires()
+                  .IsNotNull(Valor_Distribuido, nameof(Valor_Distribuido), " O valor a ser distribuido não pode ser nulo"));
+
+        }
+    }
+}
