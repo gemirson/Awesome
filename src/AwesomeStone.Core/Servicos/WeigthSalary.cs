5eb9db6dcd324915122371dbd1ffd9517eeae944
Gemirson <gemirson.silva@gmail.com>
2020-12-15 21:12:23 -0300

    feature: ensure WeigthSalary

---
+++ src/AwesomeStone.Core/Servicos/WeigthSalary.cs
@@ -0,0 +1,27 @@
+﻿using AwesomeStone.Core.Intefaces.Employees;
+using System.Linq;
+
+namespace AwesomeStone.Core.Servicos
+{
+    public class WeigthSalary:IWeigth
+    {
+            private static readonly WeigthCondition[] Transformations =
+        new WeigthCondition[]
+        {
+   
+        new WeigthCondition(m => m > 8, m => 5 ),
+        new WeigthCondition(m => m < 8 && m > 5, m=>3),
+        new WeigthCondition(m => m < 5 && m > 3, m => 2),
+        new WeigthCondition(m => m <=3 && m > 0, m => 1),
+                                                           
+    
+        };
+
+        public int GetWeigth(float parameter)
+        {
+            var executor = Transformations.First(t => t.CanApply(parameter));
+            return executor.Transform(parameter);
+        }
+            
+    }
+}
