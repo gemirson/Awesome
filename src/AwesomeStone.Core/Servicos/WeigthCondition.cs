87df2dc127a9d3d7e4579e70b0e08dfaa28bdc23
Gemirson <gemirson.silva@gmail.com>
2020-12-15 21:08:50 -0300

    feature: ensure WeigthCondition to Table

---
+++ src/AwesomeStone.Core/Servicos/WeigthCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeStone.Core.Servicos
+{
+    public class WeigthCondition
+    {
+        public Func<float, bool> CanApply { get; private set; }
+
+        public Func<float, int> Transform { get; private set; }
+
+        public WeigthCondition(Func<float, bool> _canApply, Func<float, int> _transform)
+        {
+            CanApply = _canApply;
+            Transform = _transform;
+        }
+    }
+}
