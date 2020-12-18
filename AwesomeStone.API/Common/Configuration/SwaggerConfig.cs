d4f64fa8527d242e60caaa64474a70ff1ab1ed2c
Gemirson <gemirson.silva@gmail.com>
2020-12-17 21:13:09 -0300

    feature: ensure SwaggerConfig

---
+++ AwesomeStone.API/Common/Configuration/SwaggerConfig.cs
@@ -0,0 +1,42 @@
+﻿using AwesomeStone.API.Common.Filter;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace AwesomeStone.API.Common.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Awesome",
+                    Version = "v1",
+                    Description = "Awesome",
+                });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Por favor insira um JWT com \"Bearer\" nesse campo",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<AuthOperationFilter>();
+
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+
+            return services;
+        }
+    }
+}
