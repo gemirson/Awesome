fc2c1c4d174a762925d24272c4c5a95d0bffd5a1
Gemirson <gemirson.silva@gmail.com>
2020-12-19 15:26:09 -0300

    feature:add Operation_ProfitRequest class

---
+++ AwesomeStone.Application/Command/Request/Operation_ProfitRequest.cs
@@ -0,0 +1,19 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace AwesomeStone.Application.Command.Request
+{
+    public class Operation_ProfitRequest : Notifiable
+    {
+        public string Bonus_Distribuided { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                  .Requires()
+                  .IsNotNull(Bonus_Distribuided, nameof(Bonus_Distribuided), "O valor a ser distriduido não pode ser nula"));
+        }
+    }
+}
+
+   
