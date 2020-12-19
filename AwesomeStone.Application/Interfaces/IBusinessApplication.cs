69d811e637254acaa845d1ff9d2d40c570a450cf
Gemirson <gemirson.silva@gmail.com>
2020-12-19 16:24:56 -0300

    feature:add interface IBusinessApplication

---
+++ AwesomeStone.Application/Interfaces/IBusinessApplication.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeStone.Application.Command.Request;
+using AwesomeStone.Core.Response;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AwesomeStone.Application.Interfaces
+{
+    public interface IBusinessApplication
+    {
+        ResponseResult Add(Operation_ProfitRequest operation_ProfitRequest);
+       
+    }
+}
