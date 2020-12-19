18da4431a3b0fd7881fe4bbeac0a6a86eabfa2b1
Gemirson <gemirson.silva@gmail.com>
2020-12-19 15:20:18 -0300

    feature: add Class Data result to return add Employee and Viewm model result

---
+++ AwesomeStone.Application/DTOs/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeStone.Application.DTOs
+{
+    internal class Data
+    {
+        public IEnumerable<ViewParticipation> participações { get;set;}
+        public string total_de_funcionarios       { get; set; }
+        public string total_distribuido           { get; set; }
+        public string total_disponibilizado       { get; set; }
+        public string saldo_total_disponibilizado { get; set; }
+    }
+}
---
+++ AwesomeStone.Application/DTOs/ViewParticipation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeStone.Application.DTOs
+{
+    struct  ViewParticipation
+    {
+        public string matricula { get; set; }
+        public string nome { get; set; }
+        public string valor_da_participação { get; set; }
+    }
+}
