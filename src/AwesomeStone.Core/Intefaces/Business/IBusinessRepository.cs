bf834fe281c4bb129abf85bf51583b0080826517
Gemirson <gemirson.silva@gmail.com>
2020-12-19 15:21:29 -0300

    feature: add interface IBusinessRepository

---
+++ src/AwesomeStone.Core/Intefaces/Business/IBusinessRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeStone.Core.Intefaces.Business
+{
+    public interface IBusinessRepository
+    {
+        void Add(string key, string value);
+        string GetAll(string key);
+    }
+}
