621d6c96ecc0b009476f31d3655c3783d678c1ea
Gemirson <gemirson.silva@gmail.com>
2020-12-19 15:22:19 -0300

    feature: add class BusinessRepository

---
+++ AwesomeStone.Infra/Repository/BusinessRepository.cs
@@ -0,0 +1,27 @@
+﻿using AwesomeStone.Core.Intefaces.Business;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Threading.Tasks;
+
+namespace AwesomeStone.Infra.Repository
+{
+    public class BusinessRepository : IBusinessRepository
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public BusinessRepository(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public void Add(string key,string value)
+        {
+            _memoryCache.Set(key,value);
+        }
+
+        public string GetAll(string key)
+        {
+           return  (string)_memoryCache.Get(key);
+        }
+    }
+}
