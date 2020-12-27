7026392df2f77c0941a13668e88a4d20fd9d1d42
Gemirson <gemirson.silva@gmail.com>
2020-12-27 10:15:06 -0300

    refactor:added a container for adaptation in the EmployeeApplication class
    added a container to suit the EmployeeApplication class for clean code. The builder of the class was very dependent, giving the impression of many responsibilities

---
+++ src/AwesomeStone.Application/ApplicationEmployeeContainer.cs
@@ -0,0 +1,20 @@
+﻿using AwesomeStone.Application.Interfaces;
+using AwesomeStone.Infra.Data.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AwesomeStone.Application
+{
+    public class ApplicationEmployeeContainer: IApplicationEmployeeContainer
+    {
+        public ILogger<EmployeeApplication> Logger { get; private set; }
+        public IUnitOfWork UnitOfWork { get;private set;}
+
+        public ApplicationEmployeeContainer(ILogger<EmployeeApplication> logger, IUnitOfWork unitOfWork)
+        {
+            Logger = logger;
+            UnitOfWork = unitOfWork;
+        }
+
+
+    }
+}
---
+++ src/AwesomeStone.Application/Interfaces/IApplicationEmployeeContainer.cs
@@ -0,0 +1,12 @@
+﻿using AwesomeStone.Infra.Data.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AwesomeStone.Application.Interfaces
+{
+    public interface IApplicationEmployeeContainer
+    {
+         ILogger<EmployeeApplication> Logger { get; }
+         IUnitOfWork UnitOfWork { get; }
+        
+    }
+}
